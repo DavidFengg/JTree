ce826842af6c00588fa86928df4adc2420160fcf
mitch-strong <themitchstrong@gmail.com>
2018-01-31 16:09:11 -0500

    Change file reference

--- conf/config.go
+++ conf/config.go
@@ -3,19 +3,20 @@ package conf
 import (
 	"io/ioutil"
 	"log"
+	"path/filepath"
 
 	yaml "gopkg.in/yaml.v2"
 )
 
 //Conf is an object created by the conf.yaml file
 type Conf struct {
-	collections []string `yaml:"collections"`
+	collection []string
 }
 
 //GetConf fills the conf struct
 func (c *Conf) GetConf() *Conf {
-
-	yamlFile, err := ioutil.ReadFile("./conf/conf.yaml")
+	path, _ := filepath.Abs("../../conf/conf.yaml")
+	yamlFile, err := ioutil.ReadFile(path)
 	if err != nil {
 		log.Printf("yamlFile.Get err   #%v ", err)
 	}
