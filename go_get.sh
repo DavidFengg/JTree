1e48b7ee5a5eeba507f7181f6cc8be27131b63a8
mitch-strong <themitchstrong@gmail.com>
2018-02-01 17:15:13 -0500

    Added bash script that updates local packages for developement

---
+++ go_get.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+go get -u github.com/CanDIG/candig_mds/tests
+go get -u github.com/CanDIG/candig_mds/repos
+go get -u github.com/CanDIG/candig_mds/restapi
+go get -u github.com/CanDIG/candig_mds/database
+go get -u github.com/CanDIG/candig_mds/conf
+go get -u github.com/CanDIG/candig_mds/restapi/operations
