466d22cd7a7551937c1016d7cfb0911193b48096
mitch-strong <themitchstrong@gmail.com>
2018-02-12 15:21:40 -0500

    update goget

--- go_get.sh
+++ go_get.sh
@@ -5,3 +5,10 @@ go get -u github.com/go-sql-driver/mysql
 go get -u github.com/jessevdk/go-flags
 go get -u github.com/jmoiron/sqlx
 go get -u github.com/tylerb/graceful
+
+go get -u github.com/go-openapi/errors
+go get -u github.com/go-openapi/loads
+go get -u github.com/go-openapi/runtime
+go get -u github.com/go-openapi/spec
+go get -u github.com/go-openapi/strfmt
+go get -u github.com/go-openapi/swag
