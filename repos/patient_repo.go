39ba01bddacf8b732cc892e1a1048301c4eb84ac
mitch-strong <themitchstrong@gmail.com>
2018-02-13 15:36:11 -0500

    Change case

--- repos/patient_repo.go
+++ repos/patient_repo.go
@@ -36,7 +36,7 @@ func GetPatientColumns() []string {
 
 //InsertPatient allows users to add generic objects to a collection in the database
 func InsertPatient(person *models.Patient) bool {
-	stmt, err := database.DB.Prepare("INSERT INTO `Patients`(`first_name`,`last_name`,`initials`,`gender`,`mrn`,`dob`,`on_hcn`,`clinical_history`,`patient_type`,`se_num`,`patient_id`,`sample_id`,`date_received`,`referring_physican`,`date_reported`,`surgical_date`)VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);")
+	stmt, err := database.DB.Prepare("INSERT INTO `patients`(`first_name`,`last_name`,`initials`,`gender`,`mrn`,`dob`,`on_hcn`,`clinical_history`,`patient_type`,`se_num`,`patient_id`,`sample_id`,`date_received`,`referring_physican`,`date_reported`,`surgical_date`)VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);")
 	if err != nil {
 		log.Fatal(err)
 	}
