8e91664a4ed2bca17ca4d39de1876a4947856d4f
mitch-strong <themitchstrong@gmail.com>
2018-02-21 17:27:26 -0500

    Comment out, should fail

--- tests/unit_test.go
+++ tests/unit_test.go
@@ -4,55 +4,50 @@ import (
 	"bytes"
 	"encoding/json"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 	"testing"
 
 	models "github.com/Bio-core/jtree/models"
-	"github.com/Bio-core/jtree/restapi"
-	"github.com/Bio-core/jtree/restapi/operations"
-	"github.com/go-openapi/loads"
-	flags "github.com/jessevdk/go-flags"
 )
 
 const server = "http://127.0.0.1:8000"
 
 func TestMain(m *testing.M) {
-	swaggerSpec, err := loads.Embedded(restapi.SwaggerJSON, restapi.FlatSwaggerJSON)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	// swaggerSpec, err := loads.Embedded(restapi.SwaggerJSON, restapi.FlatSwaggerJSON)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
 
-	api := operations.NewJtreeMetadataAPI(swaggerSpec)
-	server := restapi.NewServer(api)
-	defer server.Shutdown()
+	// api := operations.NewJtreeMetadataAPI(swaggerSpec)
+	// server := restapi.NewServer(api)
+	// defer server.Shutdown()
 
-	parser := flags.NewParser(server, flags.Default)
-	parser.ShortDescription = "Jtree Metadata API"
-	parser.LongDescription = "Metadata API"
+	// parser := flags.NewParser(server, flags.Default)
+	// parser.ShortDescription = "Jtree Metadata API"
+	// parser.LongDescription = "Metadata API"
 
-	server.ConfigureFlags()
-	for _, optsGroup := range api.CommandLineOptionsGroups {
-		_, err := parser.AddGroup(optsGroup.ShortDescription, optsGroup.LongDescription, optsGroup.Options)
-		if err != nil {
-			log.Fatalln(err)
-		}
-	}
+	// server.ConfigureFlags()
+	// for _, optsGroup := range api.CommandLineOptionsGroups {
+	// 	_, err := parser.AddGroup(optsGroup.ShortDescription, optsGroup.LongDescription, optsGroup.Options)
+	// 	if err != nil {
+	// 		log.Fatalln(err)
+	// 	}
+	// }
 
-	if _, err := parser.Parse(); err != nil {
-		code := 1
-		if fe, ok := err.(*flags.Error); ok {
-			if fe.Type == flags.ErrHelp {
-				code = 0
-			}
-		}
-		os.Exit(code)
-	}
+	// if _, err := parser.Parse(); err != nil {
+	// 	code := 1
+	// 	if fe, ok := err.(*flags.Error); ok {
+	// 		if fe.Type == flags.ErrHelp {
+	// 			code = 0
+	// 		}
+	// 	}
+	// 	os.Exit(code)
+	// }
 
-	server.ConfigureAPI()
-	//server.Host = "127.0.0.1"
-	go server.Serve()
+	// server.ConfigureAPI()
+	// //server.Host = "127.0.0.1"
+	// go server.Serve()
 
 	testResults := m.Run()
 	if !tearDown() {
