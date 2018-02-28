1b09f702d5fb4d41eb2863380a22a452b8b8a818
“mitch-strong” <themitchstrong@gmail.com>
2018-02-28 09:50:50 -0500

    fixed dep file

--- go_get.sh
+++ go_get.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 go get -u github.com/Bio-core/keycloakgo
-go get -u github.com/go-openapi
 go get -u github.com/go-sql-driver/mysql
 go get -u github.com/jessevdk/go-flags
 go get -u github.com/jmoiron/sqlx
