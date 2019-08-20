9f0b8f8a67c6d5613dbb8c60ccbbe584eb8f9a33
jinwo-o <jinwoo.hwang99@gmail.com>
2019-08-20 14:40:50 -0400

    Trying to package dummydata.go

--- go_get.sh
+++ go_get.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 go get -u github.com/Bio-core/keycloakgo
+go get -u github.com/Bio-core/jtree/dummydata
 go get -u github.com/go-sql-driver/mysql
 go get -u github.com/jessevdk/go-flags
 go get -u github.com/jmoiron/sqlx
